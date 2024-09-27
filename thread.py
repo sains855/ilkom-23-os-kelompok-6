f43cd2914007b39a7a46a23f3647b9215631aa8a
Ryian-hsr <muhammadryiandzuhri@gmail.com>
2024-09-27 14:39:27 +0800

    Contoh Thread dengan python

---
+++ thread.py
@@ -0,0 +1,13 @@
+import threading
+import time 
+
+def worker(thread_name):
+    print (f'Thread {thread_name} mulai bekerja')
+    time.sleep(2)
+    print(f'Thread {thread_name} selesai.')
+
+thread1 = threading.Thread(target=worker, args=('A'))
+thread2 = threading.Thread(target=worker, args=('B'))
+
+thread1.start()
+thread2.start()
